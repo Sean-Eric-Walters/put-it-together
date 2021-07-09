a6f2987ccb605ce8e68b31b9330a4f358fb3cdbe
Sean-Eric-Walters <seanericwalters@gmail.com>
2021-07-09 08:58:35 -0700

    ch4

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import PersonCard from './components/PersonCard.js';
 function App() {
   return (
     <div className="App">
-      <PersonCard last_name="Walters" first_name="Sean" age={45} hair_color="Black"/>
+      <PersonCard last_name="Walters" first_name="Sean" age={45} hair_color="Red"/>
       <PersonCard last_name="Murphy" first_name="Tara" age={38} hair_color="Brown"/>
-      <PersonCard last_name="Walters" first_name="Ronan" age={4} hair_color="Brown"/>
-      <PersonCard last_name="Bobo" first_name="Friendly" age={1} hair_color="Brown"/>
+      <PersonCard last_name="Walters" first_name="Ronan" age={4} hair_color="Red/Blond"/>
+      <PersonCard last_name="Bobo" first_name="Friendly" age={1} hair_color="Blue"/>
     </div>
   );
 }
